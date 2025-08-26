d69dcce5762dc36b2ce894c2b89c2877bd1d5c79
Lairton <130435095+Laiirton@users.noreply.github.com>
2025-08-26 16:15:56 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+
+    Route::middleware('auth.jwt')->group(function () {
+        Route::get('/me', [AuthController::class, 'me']);
+    });
+});
 
 Route::middleware('auth.jwt')->group(function () {
-    Route::get('/me', [AuthController::class, 'me']);
-
-    // Users CRUD
-    Route::get('/users', [UserController::class, 'index']);
-    Route::get('/users/{id}', [UserController::class, 'show']);
-    Route::post('/users', [UserController::class, 'store']);
-    Route::put('/users/{id}', [UserController::class, 'update']);
-    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+    Route::apiResource('users', UserController::class)->parameters(['users' => 'id']);
 });
