9dce2d7e7b5e310d3ba33c674367dc291a209e71
Lairton <130435095+Laiirton@users.noreply.github.com>
2025-09-03 14:35:34 -0300

    Update services.php

--- config/services.php
+++ config/services.php
@@ -35,4 +35,14 @@ return [
         ],
     ],
 
+    'supabase' => [
+        'url' => env('SUPABASE_URL'),
+        'endpoint' => env('SUPABASE_URL') . '/storage/v1/s3',
+        'public_url' => env('SUPABASE_URL'),
+        'access_key_id' => env('SUPABASE_ACCESS_KEY_ID'),
+        'secret_access_key' => env('SUPABASE_SECRET_ACCESS_KEY'),
+        'region' => env('SUPABASE_REGION', 'sa-east-1'),
+        'bucket' => env('SUPABASE_BUCKET', 'api'),
+    ],
+
 ];
