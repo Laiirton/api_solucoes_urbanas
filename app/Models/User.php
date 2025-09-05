1fd77da8bc78f2909a0b77b37b00ff11cf708256
Lairton <130435095+Laiirton@users.noreply.github.com>
2025-09-05 15:10:22 -0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -61,5 +61,10 @@ class User extends Authenticatable
             ->map(fn ($word) => Str::substr($word, 0, 1))
             ->implode('');
     }
+
+    public function serviceRequests()
+    {
+        return $this->hasMany(ServiceRequest::class);
+    }
 }
 
